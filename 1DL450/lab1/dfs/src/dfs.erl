99d377f74ba44e03838ad508db9397e5c1dce7cb
Henrik Arro <henrik.arro@gmail.com>
2017-11-07 12:25:47 +0100

    Added Dialyzer flag to check type specs; fixed type specs for powerset and dfs

--- 1DL450/lab1/dfs/src/dfs.erl
+++ 1DL450/lab1/dfs/src/dfs.erl
@@ -2,13 +2,20 @@
 
 -export([complete_binary_tree/1, search/2]).
 
+-type tree(T) :: empty_tree(T) | non_empty_tree(T).
+-type empty_tree(_T) :: {}.
+-type non_empty_tree(T) :: {T, tree(T), tree(T)}.
+
+-spec complete_binary_tree(pos_integer()) -> non_empty_tree(pos_integer()).
 complete_binary_tree(Depth) -> complete_binary_tree(Depth, 1, 1).
 
+%-spec complete_binary_tree(pos_integer(), pos_integer(), L) -> non_empty_tree(L) when L :: pos_integer().
 complete_binary_tree(MaxDepth, MaxDepth, Label) -> {Label, {}, {}};
 complete_binary_tree(MaxDepth, CurrentDepth, Label) ->
     {Label, complete_binary_tree(MaxDepth, CurrentDepth + 1, Label * 2),
      complete_binary_tree(MaxDepth, CurrentDepth + 1, Label * 2 + 1)}.
 
+-spec search(non_empty_tree(L), any()) -> [L, ...].
 search({}, _N) -> throw(not_found);
 search({Label, _Left, _Right}, N) when Label =:= N -> [Label];
 search({Label, Left, Right}, N) ->
--- 1DL450/lab1/powerset/src/powerset.erl
+++ 1DL450/lab1/powerset/src/powerset.erl
@@ -2,14 +2,21 @@
 
 -export([powerset/1]).
 
+%% @doc Gives the powerset of the given set, i.e., a set with all subsets of the set.
+-spec powerset(sets:set(T)) -> sets:set(sets:set(T)).
 powerset(Set) ->
     sets:from_list(powerset_list(sets:to_list(Set))).
 
--spec powerset_list([T]) -> [[T]].
+-spec powerset_list([T]) -> [[T], ...].
 powerset_list([]) -> [[]];
 powerset_list([E|T]) ->
     PT = powerset_list(T),
     PT ++ f(E, PT).
-                     
+
+%% @doc Adds an element E to each list in T.
+%%
+% %% @reference See <a href="https://en.wikipedia.org/wiki/Power_set#Algorithms">
+% %% Wikipedia</a> for more information.
+-spec f(T, [[T], ...]) -> [[T, ...]].
 f(E, T) ->
     [[E|X] || X <- T].
