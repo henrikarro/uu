19deb91876b18d33d0f9848b1b0304c28be61184
Henrik Arro <henrik.arro@svenskakyrkan.se>
2017-11-10 16:24:33 +0100

    Updated calc_pi.erl: now uses Schedulers * 4 worker processes; added type specifications and EDoc

--- 1DL450/assignment1/calc_pi.erl
+++ 1DL450/assignment1/calc_pi.erl
@@ -2,17 +2,30 @@
 
 -export([calc/2, monte_carlo/2]).
 
+%% @doc
+%% Runs a Monte Carlo simulation to approximate Pi, using a total of
+%% N simulation steps spread between Schedulers * 4 number of processes.
+-spec calc(pos_integer(), pos_integer()) -> float().
 calc(N, Schedulers) ->
-    spawn_workers(round(N / Schedulers), Schedulers),
-    C = receive_replies(Schedulers, 0),
+    NumWorkers = Schedulers * 4,
+    spawn_workers(round(N / NumWorkers), NumWorkers),
+    C = receive_replies(NumWorkers, 0),
     4 * C / N.
 
+%% @doc
+%% Creates NumWorkers new processes that each calls monte_carlo(N, 0)
+%% and sends a message back to the creating process with the result.
+-spec spawn_workers(pos_integer(), non_neg_integer()) -> ok.
 spawn_workers(_N, 0) -> ok;
 spawn_workers(N, NumWorkers) ->
     Pid = self(),
     spawn(fun() -> C = monte_carlo(N, 0), Pid ! {monte_carlo, C} end),
     spawn_workers(N, NumWorkers - 1).
 
+%% @doc
+%% Waits for replies from NumWorkers processes and returns the sum
+%% of the monte_carlo/2 results from each process (plus C).
+-spec receive_replies(non_neg_integer(), integer()) -> integer().
 receive_replies(0, C) -> C;
 receive_replies(NumWorkers, C) ->
     receive
@@ -20,6 +33,11 @@ receive_replies(NumWorkers, C) ->
             receive_replies(NumWorkers - 1, C + NewC)
     end.
 
+%% @doc
+%% Runs a Monte Carlo simulation with N random points with coordinates
+%% between 0 and 1 and returns the number of points that are within the
+%% unit circle.
+-spec monte_carlo(non_neg_integer(), integer()) -> integer().
 monte_carlo(0, C) -> C;
 monte_carlo(N, C) ->
     {X, Y} = random_point(),
@@ -30,6 +48,10 @@ monte_carlo(N, C) ->
             monte_carlo(N - 1, C)
     end.
 
+%% @doc
+%% Creates a point {X, Y} with random X and Y coordinates between
+%% zero (inclusive) and one (exclusive).
+-spec random_point() -> {float(), float()}.
 random_point() ->
     X = rand:uniform(),
     Y = rand:uniform(),
