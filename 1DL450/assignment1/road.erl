184ff0a3aa89f8077c0fd14b56d63ca29850328e
Henrik Arro <henrik.arro@svenskakyrkan.se>
2017-11-10 15:13:45 +0100

    Added missing type specifications to road.erl; add @doc; updated Makefile to generate EDoc for private functions

--- 1DL450/assignment1/road.erl
+++ 1DL450/assignment1/road.erl
@@ -32,6 +32,7 @@ days(Length, [Interval|Intervals], Goal, IntervalsSoFar, Day) ->
         false -> Day
     end.
 
+%% @doc
 %% Adds an interval to a list of intervals which is supposed to be sorted
 %% by the lower limits, and with no overlapping intervals. The new interval
 %% is merged with neighboring intervals if possible, and the resulting
@@ -52,6 +53,7 @@ add_interval({L1,H1}, [{L2,H2}|Intervals]) when L1 =< L2 andalso H1 >= H2 ->
 add_interval({L1,H1}, [{L2,H2}|Intervals]) when L1 >= L2 andalso H1 =< H2 ->
     [{L2,H2}|Intervals].
 
+%% @doc
 %% Determines if a task of length Length can be successfully scheduled in
 %% a time window defined by Start and End, and with other tasks defined by
 %% a list of intervals already scheduled.
@@ -62,11 +64,11 @@ is_schedulable(Length, [{L,H}|Intervals], Start, End) when Start =< L ->
     L - Start + 1 >= Length orelse is_schedulable(Length, Intervals, H + 1, End);
 is_schedulable(Length, [{L,H}|Intervals], Start, End) when Start > L ->
     is_schedulable(Length, Intervals, H + 1, End).
-    
+
 %%--------------------------------------------------------------------
 %% EUnit Test Cases
 %%--------------------------------------------------------------------
- 
+
 days_assignment_example_test_() ->
     [
      ?_assertEqual(-1, road:days(30, [{1,5},{11,27},{2,14},{18,28}], 0)),
@@ -133,6 +135,7 @@ prop_add_intervals_is_sorted_with_no_overlap() ->
 interval() ->
     ?LET(Lower, range(1, 900), {Lower, Lower + rand:uniform(500)}).
 
+%% @doc
 %% Verifies that the result of days/3 is correct.
 -spec verify_result(pos_integer(), [interval()], non_neg_integer(), integer()) -> boolean().
 verify_result(Length, Intervals, Goal, Result) when Result =:= -1 ->
@@ -143,22 +146,24 @@ verify_result(Length, Intervals, Goal, Result) when Result > 0 ->
     longest_interval(invert_intervals(add_n_intervals(Result, Intervals), 0, Length)) =< Goal andalso
         longest_interval(invert_intervals(add_n_intervals(Result - 1, Intervals), 0, Length)) > Goal.
 
+%% @doc
 %% An alternate implementation of days/3 that is slower but uses a simpler algorithm.
 -spec days_alt(pos_integer(), [interval()], non_neg_integer()) -> integer().
 days_alt(Length, Intervals, Goal) ->
     days_alt(Length, Intervals, Goal, 0, []).
 
-days_alt(Length, [], Goal, DaysSoFar, IntervalsSoFar) ->
+days_alt(Length, [], Goal, Day, IntervalsSoFar) ->
     case longest_interval(invert_intervals(IntervalsSoFar, 0, Length)) =< Goal of
-        true -> DaysSoFar;
+        true -> Day;
         false -> -1
     end;
-days_alt(Length, [Interval|Intervals], Goal, DaysSoFar, IntervalsSoFar) ->
+days_alt(Length, [Interval|Intervals], Goal, Day, IntervalsSoFar) ->
     case longest_interval(invert_intervals(IntervalsSoFar, 0, Length)) =< Goal of
-        true -> DaysSoFar;
-        false -> days_alt(Length, Intervals, Goal, DaysSoFar + 1, add_interval(Interval, IntervalsSoFar))
+        true -> Day;
+        false -> days_alt(Length, Intervals, Goal, Day + 1, add_interval(Interval, IntervalsSoFar))
     end.
 
+%% @doc
 %% Verifies that a list of intervals is sorted by lower limit, and that there
 %% are no overlapping intervals.
 -spec is_sorted_with_no_overlaps([interval()]) -> boolean().
@@ -167,6 +172,7 @@ is_sorted_with_no_overlaps([{_L,_H}]) -> true;
 is_sorted_with_no_overlaps([{L1,H1},{L2,H2}|Intervals]) ->
     L1 < L2 andalso H1 < L2 andalso is_sorted_with_no_overlaps([{L2,H2}|Intervals]).
 
+%% @doc
 %% Inverts the intervals in the list Intervals, given that Start and End are the
 %% minimum and maximum values, respectively, of any intervals.
 %%
@@ -179,9 +185,9 @@ invert_intervals([], Start, End, Result) when Start =< End ->
     Result ++ [{Start, End}];
 invert_intervals([], Start, End, Result) when Start > End ->
     Result;
-invert_intervals([{L,H}|Intervals], Start, End, Result) when Start < L -> 
+invert_intervals([{L,H}|Intervals], Start, End, Result) when Start < L ->
     invert_intervals(Intervals, H, End, Result ++ [{Start, L}]);
-invert_intervals([{L,H}|Intervals], Start, End, Result) when Start >= L -> 
+invert_intervals([{L,H}|Intervals], Start, End, Result) when Start >= L ->
     invert_intervals(Intervals, H, End, Result).
 
 %% Gives the length of the longest interval in the list Intervals.
@@ -195,11 +201,13 @@ longest_interval([{L,H}|Intervals], Result) when H - L > Result ->
 longest_interval([{L,H}|Intervals], Result) when H - L =< Result ->
     longest_interval(Intervals, Result).
 
+%% @doc
 %% Calls add_interval/2 on the the first N intervals in the list Intervals.
 -spec add_n_intervals(non_neg_integer(), [interval()]) -> [interval()].
 add_n_intervals(N, Intervals) ->
     add_intervals(lists:sublist(Intervals, N)).
 
+%% @doc
 %% Calls add_interval/2 on all intervals in the list Intervals.
 -spec add_intervals([interval()]) -> [interval()].
 add_intervals(Intervals) -> add_intervals(Intervals, []).
@@ -218,13 +226,19 @@ add_intervals([Interval|Intervals], IntervalsSoFar) ->
 %% % Gives approximately 8000 ms on my machine
 %%--------------------------------------------------------------------
 
+%% @doc
+%% Generates N random intervals with lower bound between 1 and Max - 1, and a
+%% length between 1 and MaxLength.
+-spec generate_random_intervals(non_neg_integer(), pos_integer(), pos_integer()) -> [interval()].
 generate_random_intervals(N, Max, MaxLength) ->
     generate_random_intervals(N, Max, MaxLength, []).
 
+-spec generate_random_intervals(non_neg_integer(), pos_integer(), pos_integer(), [interval()]) -> [interval()].
 generate_random_intervals(0, _Max, _MaxLength, Intervals) -> Intervals;
 generate_random_intervals(N, Max, MaxLength, Intervals) ->
     generate_random_intervals(N - 1, Max, MaxLength, [generate_random_interval(Max, MaxLength)|Intervals]).
 
+-spec generate_random_interval(pos_integer(), pos_integer()) -> interval().
 generate_random_interval(Max, MaxLength) ->
     L = rand:uniform(Max - 1),
     H = L + rand:uniform(MaxLength),
@@ -235,6 +249,9 @@ generate_random_interval(Max, MaxLength) ->
             {L, H}
     end.
 
+%% @doc
+%% Runs a function a number of times and returns the average run time in milliseconds.
+-spec benchmark(fun(), [any()], pos_integer()) -> float().
 benchmark(Fun, ArgList, NumRuns) ->
     Rs = [timer:tc(?MODULE, Fun, ArgList) || _ <- lists:seq(1, NumRuns)],
     lists:sum([T || {T, _} <- Rs]) / (1000 * length(Rs)).
