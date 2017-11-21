d17f6e72f8cd9136b2eea530ccef246ee28c14e2
Henrik Arro <henrik.arro@gmail.com>
2017-11-21 16:15:44 +0100

    Added fib/m and simplify using macros to lab2.rkt

--- 1DL450/lab2/lab2-test.rkt
+++ 1DL450/lab2/lab2-test.rkt
@@ -13,21 +13,42 @@
 (check-equal? (qsort '(6 3 2 5 6 3 3 5 7 3 1)) '(1 2 3 3 3 3 5 5 6 6 7))
 
 (check-equal? (let ([f (lambda  (x) x)]) (f 'a)) 'a)
-
 (check-equal? (let ([f (lambda  x x)]) (f 'a)) '(a))
-
 (check-equal? (let ([f (lambda  (x . y) x)]) (f 'a)) 'a)
-
 (check-equal? (let ([f (lambda (x . y) y)]) (f 'a)) '())
 
-(check-equal? (fib 0) 0)
+(check-equal? (fib 0) 1)
 (check-equal? (fib 1) 1)
-(check-equal? (fib 2) 1)
-(check-equal? (fib 3) 2)
-(check-equal? (fib 4) 3)
-(check-equal? (fib 5) 5)
-(check-equal? (fib 6) 8)
-(check-equal? (fib 7) 13)
-(check-equal? (fib 8) 21)
-(check-equal? (fib 9) 34)
-(check-equal? (fib 10) 55)
+(check-equal? (fib 2) 2)
+(check-equal? (fib 3) 3)
+(check-equal? (fib 4) 5)
+(check-equal? (fib 5) 8)
+(check-equal? (fib 6) 13)
+(check-equal? (fib 7) 21)
+(check-equal? (fib 8) 34)
+(check-equal? (fib 9) 55)
+(check-equal? (fib 10) 89)
+
+(check-equal? (fib/m 0) 1)
+(check-equal? (fib/m 1) 1)
+(check-equal? (fib/m 2) 2)
+(check-equal? (fib/m 3) 3)
+(check-equal? (fib/m 4) 5)
+(check-equal? (fib/m 5) 8)
+(check-equal? (fib/m 6) 13)
+(check-equal? (fib/m 7) 21)
+(check-equal? (fib/m 8) 34)
+(check-equal? (fib/m 9) 55)
+(check-equal? (fib/m 10) 89)
+
+(let* ([t1 (current-milliseconds)]
+       [t2 (begin
+             (fib 30)
+             (current-milliseconds))]
+       [t3 (begin
+             (fib/m 30)
+             (current-milliseconds))])
+  (check-true (> (- t2 t1) (* (- t3 t2) 100))))
+
+(check-equal? (simp '(+ (+ y 0) (* y 1))) '(* y 2))
+(check-equal? (simp '(+ (* y 0) x)) 'x)
--- 1DL450/lab2/lab2.rkt
+++ 1DL450/lab2/lab2.rkt
@@ -2,7 +2,9 @@
 
 (provide permutation
          qsort
-         fib)
+         fib
+         fib/m
+         simp)
 
 (define (pyth)
   (for* ([a (in-range 1 20)]
@@ -28,5 +30,50 @@
         (append (qsort smaller) (list p) (qsort greater)))))
 
 (define (fib n)
-  (cond [(or (= n 0) (= n 1)) n]
+  (cond [(or (= n 0) (= n 1)) 1]
         [else (+ (fib (- n 1)) (fib (- n 2)))]))
+
+(define-syntax-rule (define-memo (f args ...) body ...)
+  (define f
+    (let ([memo (make-hash)])
+      (lambda (args ...)
+        (cond [(hash-has-key? memo (list args ...))
+               (hash-ref memo (list args ...))]
+              [else
+               (let ([res (begin body ...)])
+                 (hash-set! memo (list args ...) res)
+                 res)])))))
+
+(define-memo (fib/m x)
+  (displayln x) ; side effect to see when we actually execute the body
+  (case x
+    [(0 1) 1]
+    [else (+ (fib/m (- x 1)) (fib/m (- x 2)))]))
+
+(define-syntax simplifier
+  (syntax-rules (->)
+    [(simplifier (x -> y) ...)
+     (recur (lambda (form)
+              (match form
+                [`x `y]
+                ...
+                [else form])))]))
+
+(define ((recur simplify) form)
+  (let loop ([reduced-form
+              (cond [(list? form)
+                     (map (recur simplify) form)]
+                    [else form])])
+    (let ([final-form (simplify reduced-form)])
+      (if (equal? final-form reduced-form)
+          final-form
+          (loop final-form)))))
+
+(define simp (simplifier
+              [(+ ,x 0) -> ,x]
+              [(+ 0 ,x) -> ,x]
+              [(* ,x 0) -> 0]
+              [(* 0 ,x) -> 0]
+              [(* ,x 1) -> ,x]
+              [(* 1 ,x) -> ,x]
+              [(+ ,x ,x) -> (* ,x 2)]))
