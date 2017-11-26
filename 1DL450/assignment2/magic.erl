4cc7d6f48e2ac172ad85721afb70eb752d0027b2
Henrik Arro <henrik.arro@svenskakyrkan.se>
2017-11-26 13:16:58 +0100

    Updated magic.erl: Fixed off-by-one errors in insert and replace; added reset to new list of spells in 1 percent of the cases

--- 1DL450/assignment2/magic.erl
+++ 1DL450/assignment2/magic.erl
@@ -36,35 +36,47 @@ spell() ->
     oneof(spells:spells()).
 
 list_of_spells_next(PreviousSpells, _Temperature) ->
+    frequency([
+               {1, list_of_spells()},
+               {99, add_remove_or_replace_random_spell(PreviousSpells)}
+              ]).
+
+add_remove_or_replace_random_spell(PreviousSpells) ->
     ?LET(NextSpell, spell(),
          begin
              R = rand:uniform(),
              Spells =
                  if
                      length(PreviousSpells) > 0 andalso R < 0.3 ->
-                         lists:delete(lists:nth(rand:uniform(length(PreviousSpells)), PreviousSpells), PreviousSpells);
+                         delete(PreviousSpells, rand:uniform(length(PreviousSpells)));
                      length(PreviousSpells) > 0 andalso R < 0.7 ->
                          replace(PreviousSpells, rand:uniform(length(PreviousSpells)), NextSpell);
                      length(PreviousSpells) > 0 ->
-                         insert(PreviousSpells, rand:uniform(length(PreviousSpells)), NextSpell);
+                         insert(PreviousSpells, rand:uniform(length(PreviousSpells) + 1), NextSpell);
                      true ->
                          [NextSpell]
                  end,
+
+%             io:format("~w ~w~n", [length(PreviousSpells), length(Spells)]),
              Spells
          end).
 
 %% @doc
-%% Inserts E at position N in list L.
--spec insert(list(any()), non_neg_integer(), any()) -> list(any()).
-insert([], _, E) -> [E];
-insert(L, 0, E) -> [E|L];
+%% Deletes the element at position N (1 &#8804; N &#8804; length(L)) in list L.
+-spec delete([any(), ...], pos_integer()) -> [any()].
+delete(L, N) ->
+    lists:delete(lists:nth(N, L), L).
+
+%% @doc
+%% Inserts E at position N (1 &#8804; N &#8804; length(L)+1) in list L.
+-spec insert([any()], pos_integer(), any()) -> [any(), ...].
+insert(L, 1, E) -> [E|L];
 insert([X|Xs], N, E) -> [X|insert(Xs, N - 1, E)].
 
 %% @doc
-%% Replaces element at position N in list L with E.
--spec replace(list(any()), non_neg_integer(), any()) -> list(any()).
-replace([], _, _) -> [];
-replace([_|Xs], 0, E) -> [E|Xs];
+%% Replaces element at position N (1 &#8804; N &#8804; length(L)) in list L with E.
+-spec replace([any(), ...], pos_integer(), any()) -> [any(), ...].
+replace([_|Xs], 1, E) -> [E|Xs];
 replace([X|Xs], N, E) -> [X|replace(Xs, N - 1, E)].
 
 %% @doc
@@ -73,6 +85,7 @@ replace([X|Xs], N, E) -> [X|replace(Xs, N - 1, E)].
 -spec run_tests(non_neg_integer()) -> {ok | fail}.
 run_tests(0) -> ok;
 run_tests(N) ->
+    io:format("Running test ~w~n", [N]),
     case proper:quickcheck(prop_spells_targeted(), [{numtests, 10000}, quiet]) of
         true ->
             fail;
