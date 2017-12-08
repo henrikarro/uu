1c15bb6a82382dec604944c278a82b89e790f004
Henrik Arro <henrik.arro@gmail.com>
2017-12-08 17:39:24 +0100

    Updated road.hs to use Int instead of Integer

--- 1DL450/assignment3/road.hs
+++ 1DL450/assignment3/road.hs
@@ -6,20 +6,20 @@ main = do
   intervals <- forM [1..numLines] (const readPair)
   print (days length intervals goal)
 
-readInts :: IO [Integer]
+readInts :: IO [Int]
 readInts = fmap (map read.words) getLine
 
-readPair :: IO (Integer, Integer)
+readPair :: IO (Int, Int)
 readPair = do
   [x, y] <- readInts
   return (x, y)
 
-type Interval = (Integer, Integer)
+type Interval = (Int, Int)
 
-days :: Integer -> [Interval] -> Integer -> Integer
+days :: Int -> [Interval] -> Int -> Int
 days length intervals goal = days' length intervals goal [] 0
 
-days' :: Integer -> [Interval] -> Integer -> [Interval] -> Integer -> Integer
+days' :: Int -> [Interval] -> Int -> [Interval] -> Int -> Int
 days' length [] goal intervalsSoFar day = if isSchedulable (goal + 1) intervalsSoFar 0 length then -1 else day
 days' length (interval:intervals) goal intervalsSoFar day = if isSchedulable (goal + 1) intervalsSoFar 0 length then day' else day
   where day' = days' length intervals goal (addInterval interval intervalsSoFar) (day + 1)
@@ -32,7 +32,7 @@ addInterval (low1,hi1) ((low2,hi2):intervals) | hi1 < low2 = (low1,hi1):(low2,hi
                                               | low1 <= low2 && hi1 >= hi2 = (low1,hi1):intervals
                                               | low1 >= low2 && hi1 <= hi2 = (low2,hi2):intervals
 
-isSchedulable :: Integer -> [Interval] -> Integer -> Integer -> Bool
+isSchedulable :: Int -> [Interval] -> Int -> Int -> Bool
 isSchedulable length [] start end = end - start >= length
 isSchedulable _length _intervals start end | start >= end = False
 isSchedulable length ((low,hi):intervals) start end | start < low = low - start >= length || isSchedulable length intervals hi end
